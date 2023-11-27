e75d7eb5d3dedbae184cc26b06b11f5bd75c5193
Ibrahim <ibrahim.khareghat@dcpltechnology.com>
2023-11-27 17:56:12 +0530

    changes

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,19 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// class LoginPage extends StatefulWidget {
-//   const LoginPage({super.key});
-
-//   @override
-//   State<LoginPage> createState() => _LoginPageState();
-// }
-
-// class _LoginPageState extends State<LoginPage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold();
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
