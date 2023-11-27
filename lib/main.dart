69f05342890eaa9d3eda2a3e33de18faab6dcee4
Ibrahim <ibrahim.khareghat@dcpltechnology.com>
2023-11-27 17:49:32 +0530

    addmain

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/loginpage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
